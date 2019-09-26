484d3217704dbd0cf92358dff5fb9a21eb6267d2
kirm99 <martindale29k@gmail.com>
2019-09-26 09:40:56 -0400

    Updated signIn component to have a form to enter information.

--- homeaway/src/components/SignIn.js
+++ homeaway/src/components/SignIn.js
@@ -2,10 +2,27 @@ import React from 'react'
 
 class SignIn extends React.Component{
 
+    handleClick()
+    {
+        console.log("tried to log in")
+    }
+
     render() {
         return(
             <div className="signin">
-
+                <form>
+                    <label htmlFor="userName"><b>Username/E-mail: </b></label>
+                    <input type="text" placeholder="Enter Username" name="userName" required />
+                    <br/>
+                    <label htmlFor="password"><b>Password: </b></label>
+                    <input type="text" placeholder="Enter password" name="password" required />
+                    <br/>
+                    <a href="">Forgot Password</a>
+                    <br/>
+                    <a href="">Don't have an account Sign-up</a>
+                    <br/>
+                    <button className="logInButton" onClick={this.handleClick}>Log-in</button>
+                </form>
             </div>
         )
     }
