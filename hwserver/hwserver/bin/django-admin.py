b576eb533b67d8151f48f7b8fdab86ddafaf2cc2
alexistj <josepa2@rpi.edu>
2019-10-15 16:41:01 -0400

    inital django setup w/ instructions

---
+++ hwserver/hwserver/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/users/alexi/OneDrive/Desktop/HomeAway/hwserver/hwserver/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
