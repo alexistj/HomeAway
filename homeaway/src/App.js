9e19667077904ceaf2496b033a08ba6eb5e6a764
Jung <jungn@rpi.edu>
2019-10-18 16:47:36 -0400

    Carousel Components

--- homeaway/src/App.js
+++ homeaway/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from "./components/Header";
+import Carousel from "./components/Carousel"
 
 
 function App() {
   return (
     <div>
       <Header/>
+      <Carousel/>
     </div>
+
+
   )
 }
 
